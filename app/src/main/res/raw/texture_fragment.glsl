1a58077ec29159780e7fdbb1fccd731f8de0b8a8
yasutomo-k <yasutomo.k@gmail.com>
2015-12-17 00:26:43 +0900

    Add gl shader source code
    add opengl source program
    vertex code ->texutre_vertex.glsl
    fragment code->texture_fragment.glsl

---
+++ app/src/main/res/raw/texture_fragment.glsl
@@ -0,0 +1,10 @@
+#version 120
+
+uniform sampler2D u_Sampler;
+
+varying vec2 v_TexCoord;
+
+void main() {
+    gl_FragColor = texture2D(u_Sampler, v_TexCoord);
+}
+
---
+++ app/src/main/res/raw/texture_vertex.glsl
@@ -0,0 +1,13 @@
+#version 120
+
+attribute vec4 a_Position;
+attribute vec2 a_TexCoord;
+
+varying vec2 v_TexCoord;
+
+uniform mat4 u_MVPMatorix;
+
+void main() {
+    gl_Position = u_MVPMatrix * a_Position;
+    v_TexCoord = a_TexCoord;
+}
